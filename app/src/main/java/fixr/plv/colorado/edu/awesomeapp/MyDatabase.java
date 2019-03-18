6b4094f1a06bd5512f8e83a106ddaf1f243bd4a4
smover <sergio.mover@gmail.com>
2019-03-18 19:18:27 +0100

    Stuff

---
+++ app/src/main/java/fixr/plv/colorado/edu/awesomeapp/MyDatabase.java
@@ -0,0 +1,20 @@
+package fixr.plv.colorado.edu.awesomeapp;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+
+public class MyDatabase {
+
+  public void doQuery(SQLiteDatabase db,
+                      String[] columns) {
+
+    Cursor cursor = db.query("mytable", columns, null, null,
+        null, null, null);
+
+    while (cursor.moveToNext()) {
+      // get stuff from the cursor
+    }
+  }
+
+}
